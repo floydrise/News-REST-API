e85a45075b539716bd55b17647672bfe3a5493fb
Stefan <floydrise@gmail.com>
2024-11-26 19:04:31 +0200

    Prepare project for Supabase

--- Express/listener.js
+++
@@ -1,5 +0,0 @@
-const app = require("./app");
-
-app.listen(8000, () => {
-    console.log("Listening on port 8000");
-})
---
+++ listener.js
@@ -0,0 +1,5 @@
+const app = require("./Express/app");
+const { PORT = 8000 } = process.env;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
